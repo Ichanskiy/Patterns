afef3b4c50c3071d0be6d2a22b43e2dbd57f6de0
Ichanskiy <ichanskiy.olexandr@gmail.com>
2019-01-09 11:52:45 +0200

    add singleton

---
+++ src/com/singelton/Singleton.java
@@ -0,0 +1,20 @@
+package com.singelton;
+
+public class Singleton {
+
+    private static volatile Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class){
+                if (singleton == null) {
+                    return new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
